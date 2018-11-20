626096425264dfcc31fc9158b810f76be349347a
Marlin Davis <madavis@EMAIL.USCUPSTATE.EDU>
2018-11-20 10:27:38 -0500

    Add project files.

---
+++ FinalProjectv3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProjectv3.Startup))]
+namespace FinalProjectv3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
